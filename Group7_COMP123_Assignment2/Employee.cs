108349b1110a07c859e3729f65f76932fe628a0b
Bruno Cantanhede Morgado <bcantanh@my.centennialcollege.ca>
2021-03-03 12:48:05 -0500

    First Commit

---
+++ Group7_COMP123_Assignment2/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group7_COMP123_Assignment2
+{
+    class Employee
+    {
+        //Declare Fields
+        private int employeeUserId;
+        private string firstName;
+        private string lastName;
+        private double basicSalary = 2_000.00;
+        private double grossSales;
+        private double commissionRate = 0.2;
+
+        //Declare Properties
+        public int EmployeeUserId { get { return employeeUserId; } }
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public double BasicSalary { get { return basicSalary; } set { basicSalary = value; } } 
+        public double GrossSales { get { return grossSales; } set { grossSales = value; } }
+        public double CommissionRate { get { return commissionRate; } set { commissionRate = value; } }
+
+        //Build Methods
+
+        #region Earnings Method
+        public double Earnings()
+        {
+            return (CommissionRate * GrossSales) + BasicSalary;
+        }
+        #endregion
+
+        #region Employee Constructors
+        public Employee() {}
+        
+        public Employee(int employeeUserId,string firstName, double basicSalary)
+        {
+            this.employeeUserId = employeeUserId;
+            this.firstName = firstName;
+            this.basicSalary = basicSalary;
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ Group7_COMP123_Assignment2/EmployeeTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Group7_COMP123_Assignment2
+{
+    class EmployeeTest
+    {
+        static void Main(string[] args)
+        {
+            //Creation of the First Employee object.
+            Employee firstEmployee = new Employee();
+
+            //User's inputs for second Employee Object.
+            Write("Please inform employee's First Name: ");
+            string fname = ReadLine();
+
+            Write("Please inform employee's Last Name: ");
+            string lname = ReadLine();
+            
+            Write("Please inform employee's Base Salary: ");
+            double baseSalary = Convert.ToDouble(ReadLine());
+
+            bool testVar = false;
+            double grossSales = 0;
+            double commissionRate = 0;
+            while (!testVar)
+            {
+                try
+                {
+                    Write("Please inform employee's Gross Sales: ");
+                    grossSales = Convert.ToDouble(ReadLine());
+                    while (grossSales <= 0)
+                    {
+                        Write("Gross Sales cannot be negative. Please enter a valid amount: ");
+                        grossSales = Convert.ToDouble(ReadLine());
+                    }
+
+                    Write("Please inform employee's Commission Rate between 0.1 and 1.0: ");
+                    commissionRate = Convert.ToDouble(ReadLine());
+                    while (commissionRate < 0.1 || commissionRate > 1.0)
+                    {
+                        Write("Please enter a number between 0.1 and 1.0: ");
+                        commissionRate = Convert.ToDouble(ReadLine());
+                    }
+
+                    testVar = true;
+                }
+                catch (Exception e)
+                {
+                    WriteLine($"\nError: {e.Message}\n");
+                }
+
+            }
+
+           
+
+            //Create Second Employee object and assign values to fields.
+            Employee secondEmployee = new Employee(10, fname, baseSalary);
+            secondEmployee.CommissionRate = commissionRate;
+            secondEmployee.GrossSales = grossSales;
+            secondEmployee.LastName = lname;
+
+            //Calculate total Earnings.
+            double earnings = secondEmployee.Earnings();
+
+            //Output Employee's basic information and total earnings.
+            WriteLine($"\nEmployee {secondEmployee.LastName}, {secondEmployee.FirstName}, ID# {secondEmployee.EmployeeUserId}" +
+                $" has total earnings of {earnings:c}");
+
+
+        }
+    }
+}
